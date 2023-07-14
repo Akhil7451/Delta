8a9df50a042807a61c19158115237dc4a2a3c12c
Akhil Kumar <89895015+Akhil7451@users.noreply.github.com>
2023-07-14 19:50:51 +0530

    Add files via upload

---
+++ JavaScript(Part 1)/practiceQuestion.js
@@ -0,0 +1,33 @@
+//  Practice Question. 
+
+// Q-1
+let name = "Akhil Kumar";
+undefined
+name.length;
+11
+
+// Q-2
+let firstName = "Akhil";
+undefined
+name[0];
+'A'
+
+// Q-3
+firstName[firstName.length-1];
+'l'
+
+// Q-4
+"apnacollege"+123
+'apnacollege123'
+
+// Q-5
+let str = "";
+undefined
+str.length;
+0
+
+// Q-6
+let str1 = " ";
+undefined
+str1.length;
+1
